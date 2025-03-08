4132ff6c1dcdaceeed2a182d6f78491a49e7d784
AMIRAMOHAMED44 <103863618+AMIRAMOHAMED44@users.noreply.github.com>
2025-03-09 01:13:42 +0200

    add all views to trainee app

--- day1/ITI/trainee/models.py
+++ day1/ITI/trainee/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Trainee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    age = models.PositiveIntegerField()
+    course = models.CharField(max_length=100)
+    joined_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
--- day1/ITI/trainee/views.py
+++ day1/ITI/trainee/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Trainee
+from .forms import TraineeForm
+
+def trainee_list(request):
+    trainees = Trainee.objects.all()
+    return render(request, 'trainee/trainee_list.html', {'trainees': trainees})
+
+def add_trainee(request):
+    if request.method == 'POST':
+        form = TraineeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('trainee_list')
+    else:
+        form = TraineeForm()
+    return render(request, 'trainee/trainee_form.html', {'form': form})
+
+def update_trainee(request, trainee_id):
+    trainee = get_object_or_404(Trainee, id=trainee_id)
+    if request.method == 'POST':
+        form = TraineeForm(request.POST, instance=trainee)
+        if form.is_valid():
+            form.save()
+            return redirect('trainee_list')
+    else:
+        form = TraineeForm(instance=trainee)
+    return render(request, 'trainee/trainee_form.html', {'form': form})
+
+def delete_trainee(request, trainee_id):
+    trainee = get_object_or_404(Trainee, id=trainee_id)
+    if request.method == 'POST':
+        trainee.delete()
+        return redirect('trainee_list')
+    return render(request, 'trainee/trainee_confirm_delete.html', {'trainee': trainee})
