62d5a1f29c184d2df11a6745e2d6036578602334
AMIRAMOHAMED44 <103863618+AMIRAMOHAMED44@users.noreply.github.com>
2025-03-17 22:58:22 +0200

    update login and add logout, register pages

--- day1/ITI/user/urls.py
+++ day1/ITI/user/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-# from .views import registerview
+from .views import register
 from django.contrib.auth.views import LoginView , LogoutView
+from .views import custom_logout
 
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='user/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    # path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', custom_logout, name='logout'),
+    path('register/', register, name='register'),
+
 ]
+
--- day1/ITI/user/views.py
+++ day1/ITI/user/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.contrib.auth import logout
+from django.shortcuts import redirect,render
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
+
+def custom_logout(request):
+    logout(request)
+    return redirect('login')
+
+def register(req):
+    context={'form':UserCreationForm()}
+    if(req.method=='POST'):
+        form=UserCreationForm(data=req.POST)
+        if(form.is_bound and form.is_valid()):
+            form.save()
+            return redirect('login')
+        else:
+            context['error']=form.errors
+    return render(req,'user/register.html',context)
